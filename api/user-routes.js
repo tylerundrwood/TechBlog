b02e29bdec3b71e202ad9e6e5b7fd731cea52573
Tyler Underwood <twood9600@yahoo.com>
2023-07-22 16:11:07 -0400

    added more js

--- api/user-routes.js
+++ api/user-routes.js
@@ -46,3 +46,53 @@ router.get('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post('/', (req, res) => {
+
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(userData => {
+        req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.username = userData.username;
+          req.session.loggedIn = true;
+    
+          res.json(userData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(userData => {
+      if (!userData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.username = userData.username; 
+        req.session.loggedIn = true;
+    
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+    });
+  });
