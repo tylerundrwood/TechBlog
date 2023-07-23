88419ee5c1bb2313d69ec3b47e8567877621acea
Tyler Underwood <twood9600@yahoo.com>
2023-07-23 17:47:12 -0400

    added controllers

---
+++ public/controllers/home-routes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+// get all posts for homepage
+router.get('/', (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    attributes: [
+      'id',
+//      'url',
+      'title',
+      'body',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(postData => {
+      const posts = postData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
