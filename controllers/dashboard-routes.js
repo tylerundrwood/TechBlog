631aba8b90aaa31ffcd17e03a3985b817251d1de
Tyler Underwood <twood9600@yahoo.com>
2023-07-24 16:29:37 -0400

    finished dash and home

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -40,3 +40,46 @@ router.get('/', withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      // 'url',
+      'title',
+      'body',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(postData => {
+      if (postData) {
+        const post = postData.get({ plain: true });
+        
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
