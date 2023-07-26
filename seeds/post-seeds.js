f405b8fcd8515dbd2321240e04768b1d9e7ee2f3
Tyler Underwood <twood9600@yahoo.com>
2023-07-26 17:20:08 -0400

    made post seeds

---
+++ seeds/post-seeds.js
@@ -0,0 +1,25 @@
+const { Post } = require('../models');
+
+const postData = [{
+        title: 'Title One',
+        body: 'Praesent ultrices orci ut augue condimentum eleifend.',
+
+        user_id: 1
+    },
+    {
+        title: 'Title Two',
+        body: 'Sed feugiat leo sit amet libero finibus euismod. Nulla viverra vulputate eleifend.',
+
+        user_id: 2
+    },
+    {
+        title: 'Another Title',
+        body: 'Donec vitae enim in nulla tincidunt malesuada. Duis ac lacus mauris. Suspendisse non mi malesuada erat egestas porttitor id a turpis. Vivamus id libero urna. Nam tincidunt quam vitae turpis sodales, a convallis odio scelerisque.',
+
+        user_id: 3
+    }
+];
+
+const seedPosts = () => Post.bulkCreate(postData);
+
+module.exports = seedPosts;
