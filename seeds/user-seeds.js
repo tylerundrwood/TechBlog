436b53478330d07ecd4bd9c5671f19006dd7d3f0
Tyler Underwood <twood9600@yahoo.com>
2023-07-26 16:27:36 -0400

    created models file

---
+++ seeds/user-seeds.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+
+const userData = [{
+        username: 'John',
+        email: 'John@example.com',
+        password: 'wheiothwaoi'
+    },
+    {
+        username: 'Pam',
+        email: 'pam@example.com',
+        password: 'zerstorer'
+    },
+    {
+        username: 'Mary',
+        email: 'mary@example.com',
+        password: 'hjgudufo'
+    }
+];
+
+const seedUsers = () => User.bulkCreate(userData, {individualHooks: true});
+
+module.exports = seedUsers;
